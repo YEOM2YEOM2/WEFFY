ac2cedcba0e0d526f46a2ee6ea8fc9409248c91f
YEONJI <kkgg54@naver.com>
2023-07-26 14:01:53 +0900

    #S09P12D107-230 #in-progress #comment chore(package.json) : 패키지 추가

---
+++ frontend/src/pages/login.js
@@ -0,0 +1,15 @@
+import styles from'./login.module.css';
+import styled from 'styled-components';
+
+const bgImgURL = `${process.env.PUBLIC_URL}/images/login.png`
+
+
+function login() {
+  return (
+    <div className="login">
+        
+    </div>
+  )
+}
+
+export default login
