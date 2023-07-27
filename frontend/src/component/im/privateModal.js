b464335a87e4ef1ebd87b50685c8d6f8c6b33e2e
silver <hara0822@naver.com>
2023-07-27 17:52:40 +0900

    #S09P12D107-234 #in-progress #comment fix(im) : fix component with im

---
+++ frontend/src/component/im/privateModal.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import styles from "./privateModal.module.css";
+
+function privateModal(props) {
+  function closeModal() {
+    props.closeModal();
+  }
+
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+}
