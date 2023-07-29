a6dd6991145bda12909ee7cd8fc47f47aa86e6ab
이서현 <kathyleesh7@gmail.com>
2023-07-30 04:20:32 +0900

    S09P12D107-204 #in-progress #comment feat(User.Dto) : UserSignInReqDto와 UserSignInReqDto 생성
    로그인을 위한 request DTO와 response DTO 생성

--- backend/src/main/java/com/weffy/user/Dto/Response/UserSignInResDto.java
+++ backend/src/main/java/com/weffy/user/Dto/Response/UserSignInResDto.java
@@ -1,2 +1,20 @@
-package com.weffy.user.Dto.Response;public class UserSigninResDto {
+package com.weffy.user.Dto.Response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserSignInResDto {
+    String userId;
+    String profile_img;
+    String token;
+
+    public UserSignInResDto of(String userId, String profile_img, String token) {
+        UserSignInResDto res = new UserSignInResDto();
+        res.userId = userId;
+        res.profile_img = profile_img;
+        res.token = token;
+        return res;
+    }
 }
