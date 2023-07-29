ac8deb5c6038080a82d31d6285154441ed06605b
이서현 <kathyleesh7@gmail.com>
2023-07-30 04:26:50 +0900

    S09P12D107-204 #in-progress #comment feat(Mattermost) : mattermost와 연동 및 mattermost.client4 로그인, 프로필 이미지 구현
    client().login()을 통해 로그인 구현, client().getUserProfileImageRoute() 를 통해 프로필 이미지 반환 구현

--- backend/src/main/java/com/weffy/mattermost/MattermostHandler.java
+++ backend/src/main/java/com/weffy/mattermost/MattermostHandler.java
@@ -1,16 +1,31 @@
 package com.weffy.mattermost;
 
+import net.bis5.mattermost.client4.ApiResponse;
 import net.bis5.mattermost.client4.MattermostClient;
+import net.bis5.mattermost.model.User;
+import org.springframework.stereotype.Component;
 
 import java.util.logging.Level;
 
+@Component
 public class MattermostHandler {
-    public static MattermostClient client() {
+
+    public MattermostClient client() {
         return MattermostClient.builder()
                 .url("https://meeting.ssafy.com")
-                .logLevel(Level.INFO)
+//                .logLevel(Level.INFO)
                 .ignoreUnknownProperties()
                 .build();
     }
+
+    public ApiResponse<User> login(String email, String password) {
+        return client().login(email, password);
+    }
+
+    public String image(String userId) {
+        return "https://meeting.ssafy.com" + client().getUserProfileImageRoute(userId);
+    }
+
+
 
 }
