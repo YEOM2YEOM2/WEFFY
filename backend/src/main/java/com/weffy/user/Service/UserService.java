0ddd0334fd20a5908f616c40df0698007e312e36
이서현 <kathyleesh7@gmail.com>
2023-07-30 04:23:46 +0900

    S09P12D107-204 #in-progress #comment feat(User) : 로그인 & 회원가입 구현
    weffy에 해당 User의 정보가 없으면 회원가입 후 로그인, 있으면 바로 로그인

--- backend/src/main/java/com/weffy/user/Service/UserService.java
+++ backend/src/main/java/com/weffy/user/Service/UserService.java
@@ -1,2 +1,11 @@
-package com.weffy.user.Service;public interface UserService {
+package com.weffy.user.Service;
+
+import com.weffy.user.Dto.Request.UserSignInReqDto;
+import com.weffy.user.Dto.Response.UserSignInResDto;
+import com.weffy.user.Entity.Role;
+
+public interface UserService {
+
+    UserSignInResDto signIn(UserSignInReqDto signinInfo, Role role);
+
 }
