791e958a0ea70b3cec59bcbfbc73168a4240b217
이서현 <kathyleesh7@gmail.com>
2023-07-30 20:51:45 +0900

    S09P12D107-204 #in-progress #comment feat(token) : refreshtoken service 생성
    accesstoken 재발급을 위한 service 생성

--- backend/src/main/java/com/weffy/token/Service/RefreshTokenService.java
+++ backend/src/main/java/com/weffy/token/Service/RefreshTokenService.java
@@ -1,2 +1,17 @@
-package com.weffy.token.service;public class RefreshTokenService {
+package com.weffy.token.Service;
+
+import com.weffy.token.Entity.RefreshToken;
+import com.weffy.token.Repository.RefreshTokenRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RefreshTokenService {
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public RefreshToken findByRefreshToken(String refreshToken) {
+        return refreshTokenRepository.findByRefreshToken(refreshToken)
+                .orElseThrow(() -> new IllegalArgumentException("Unexpected token"));
+    }
 }
