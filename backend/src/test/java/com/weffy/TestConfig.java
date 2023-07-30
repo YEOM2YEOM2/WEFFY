f351b85849fb4eca49e730142d0152f071780eec
이서현 <kathyleesh7@gmail.com>
2023-07-30 19:05:41 +0900

    S09P12D107-204 #in-progress #comment test(User) : User Signup 분리
    대부분 test에서 사용되는 user signup 부분 분리하여 추상화

--- backend/src/test/java/com/weffy/TestConfig.java
+++ backend/src/test/java/com/weffy/TestConfig.java
@@ -4,24 +4,26 @@ import com.weffy.user.Dto.Request.UserSignInReqDto;
 import com.weffy.user.Repository.UserRepository;
 import com.weffy.user.Service.UserService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class BeforeTest {
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class TestConfig {
     @Autowired
-    private UserService userService;
+    protected UserService userService;
 
     @Autowired
-    private UserRepository userRepository;
+    protected UserRepository userRepository;
 
     @Value("${mysecret.mattermost.email}")
-    private String email;
+    protected String email;
     @Value("${mysecret.mattermost.password}")
-    private String password;
+    protected String password;
 
-    private UserSignInReqDto user;
+    protected UserSignInReqDto user;
 
     @BeforeEach
     void setUp() {
--- backend/src/test/java/com/weffy/UserTest.java
+++ backend/src/test/java/com/weffy/UserTest.java
@@ -1,12 +1,9 @@
 package com.weffy;
 
-import com.weffy.user.Dto.Request.UserSignInReqDto;
 import com.weffy.user.Entity.WeffyUser;
 import com.weffy.user.Repository.UserRepository;
-import com.weffy.user.Service.UserService;
 import jakarta.transaction.Transactional;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,35 +13,20 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.Optional;
 
 @SpringBootTest
-public class UserTest {
-
-    @Autowired
-    private UserService userService;
+public class UserTest extends TestConfig{
 
     @Autowired
     private UserRepository userRepository;
 
     @Value("${mysecret.mattermost.email}")
     private String email;
-    @Value("${mysecret.mattermost.password}")
-    private String password;
-
-    private UserSignInReqDto user;
-
-    @BeforeEach
-    void setUp() {
-        user = new UserSignInReqDto();
-        user.setEmail(email);
-        user.setPassword(password);
-        userService.signIn(user, null);
-    }
 
     @Test
     @Transactional
     @DisplayName("회원가입이 성공하여야한다.")
     void createUser_O() {
-        Optional<WeffyUser> user = userRepository.findByIdentification("1ejz9ks8kpy7uyb4rsn4s44pse");
+        Optional<WeffyUser> testUser = userRepository.findByIdentification("1ejz9ks8kpy7uyb4rsn4s44pse");
         //then
-        Assertions.assertThat(user.get().getEmail()).isEqualTo(email);
+        Assertions.assertThat(testUser.get().getEmail()).isEqualTo(email);
     }
 }
