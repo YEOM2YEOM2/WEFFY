69bdb6e2e1f81b09b12d508e6d01585d708f1573
이서현 <kathyleesh7@gmail.com>
2023-07-30 20:50:58 +0900

    S09P12D107-204 #in-progress #comment feat(token) : refreshtoken dto, repository 생성
    accesstoken 재발급을 위한 dto와 repository 생성

--- backend/src/main/java/com/weffy/token/Dto/Response/CreateAccessTokenResDto.java
+++ backend/src/main/java/com/weffy/token/Dto/Response/CreateAccessTokenResDto.java
@@ -1,2 +1,10 @@
-package com.weffy.token.Dto.Response;public class CreateAccessTokenResDto {
+package com.weffy.token.Dto.Response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CreateAccessTokenResDto {
+    private String accessToken;
 }
--- backend/src/main/java/com/weffy/token/Repository/RefreshTokenRepository.java
+++ backend/src/main/java/com/weffy/token/Repository/RefreshTokenRepository.java
@@ -1,2 +1,14 @@
-package com.weffy.token.Repository;public interface RefreshTokenRepository {
+package com.weffy.token.Repository;
+
+import com.weffy.token.Entity.RefreshToken;
+import com.weffy.user.Entity.WeffyUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+    Optional<RefreshToken> findByWeffyUser(WeffyUser weffyUser);
+
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
 }
