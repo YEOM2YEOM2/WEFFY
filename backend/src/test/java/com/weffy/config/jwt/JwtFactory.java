5a495e00ee2672066b977fc0ce13d9214d39ce6e
이서현 <kathyleesh7@gmail.com>
2023-07-30 19:07:36 +0900

    S09P12D107-204 #in-progress #comment test(jwt) : generateToken()으로 토큰 생성 test
    유저 정보와 만료 기간을 전달해 토큰 생성

--- backend/src/test/java/com/weffy/config/jwt/JwtFactory.java
+++ backend/src/test/java/com/weffy/config/jwt/JwtFactory.java
@@ -1,2 +1,45 @@
-package com.weffy;public class TokenFactory {
+package com.weffy.config.jwt;
+
+import com.weffy.token.JwtProperties;
+import com.weffy.user.Entity.WeffyUser;
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.Builder;
+import lombok.Getter;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Map;
+
+@Getter
+public class JwtFactory {
+    private String subject = "test@email.com";
+    private Date issuedAt = new Date();
+    private Date expiration = new Date(new Date().getTime() + Duration.ofDays(14).toMillis());
+    private Map<String, Object> claims = Collections.emptyMap();
+
+    @Builder
+    public JwtFactory(String subject, Date issuedAt, Date expiration, Map<String, Object> claims) {
+        this.subject = subject != null ? subject : this.subject;
+        this.issuedAt = issuedAt != null ? issuedAt : this.issuedAt;
+        this.expiration = expiration != null ? expiration : this.expiration;
+        this.claims = claims != null ? claims : this.claims;
+    }
+
+    public static JwtFactory withDefaultValues() {
+        return JwtFactory.builder().build();
+    }
+
+    private String createToken(JwtProperties jwtProperties) {
+        return Jwts.builder()
+                .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
+                .setIssuer(jwtProperties.getIssuer())
+                .setIssuedAt(issuedAt)
+                .setExpiration(expiration)
+                .setSubject(subject)
+                .addClaims(claims)
+                .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecretKey())
+                .compact();
+    }
 }
--- backend/src/test/java/com/weffy/config/jwt/TokenTest.java
+++ backend/src/test/java/com/weffy/config/jwt/TokenTest.java
@@ -1,2 +1,48 @@
-package com.weffy;public class TokenTest {
+package com.weffy.config.jwt;
+
+import com.weffy.TestConfig;
+import com.weffy.token.JwtProperties;
+import com.weffy.token.TokenProvider;
+import com.weffy.user.Entity.WeffyUser;
+import com.weffy.user.Repository.UserRepository;
+import io.jsonwebtoken.Jwts;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.Duration;
+import java.util.Optional;
+
+@SpringBootTest
+public class TokenTest extends TestConfig{
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private TokenProvider tokenProvider;
+    @Autowired
+    private JwtProperties jwtProperties;
+    @Value("${mysecret.mattermost.email}")
+    private String email;
+
+    @DisplayName("generateToken() : 유저 정보와 만료 기간을 전달해 토큰을 만들 수 있다.")
+    @Test
+    void generateToken_O() {
+        //given
+        Optional<WeffyUser> weffyUser = userRepository.findByEmail(email);
+        if (weffyUser.isPresent()) {
+            //when
+            String token = tokenProvider.generateToken(weffyUser.get(), Duration.ofDays(14));
+            //then
+            String identification = Jwts.parser()
+                    .setSigningKey(jwtProperties.getSecretKey())
+                    .parseClaimsJws(token)
+                    .getBody()
+                    .get("identification", String.class);
+
+            Assertions.assertThat(identification).isEqualTo(weffyUser.get().getIdentification());
+        }
+    }
 }
