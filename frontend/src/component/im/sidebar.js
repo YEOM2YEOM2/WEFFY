baf31feb7d07a5a6c1ee573f193d33d27d8ce59b
silver <hara0822@naver.com>
2023-07-31 10:56:45 +0900

    >

--- frontend/src/component/im/sidebar.js
+++ frontend/src/component/im/sidebar.js
@@ -2,15 +2,12 @@ import * as React from "react";
 import styles from "./sidebar.module.css";
 
 //외부 라이브러리
-import Drawer from "@mui/material/Drawer";
-import Button from "@mui/material/Button";
 import List from "@mui/material/List";
 import Divider from "@mui/material/Divider";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 
 //이미지
