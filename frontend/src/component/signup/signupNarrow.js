c487af74f20f8684cf31f1e7db4bb6f1f160734c
YEONJI <kkgg54@naver.com>
2023-07-31 14:34:28 +0900

    #S09P12D107-342 #done #comment fix(signup) : signup wide form 완성

---
+++ frontend/src/component/signup/signupNarrow.js
@@ -0,0 +1,11 @@
+import styles from './signupNarrow.module.css'
+
+function SignupNarrow() {
+  return (
+    <div className={styles.narrow}>
+        
+    </div>
+  )
+}
+
+export default SignupNarrow
