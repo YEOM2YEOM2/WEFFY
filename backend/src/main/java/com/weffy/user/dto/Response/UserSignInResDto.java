2c508a32a0cd81baccebd8bb352872fa33e127b4
이서현 <kathyleesh7@gmail.com>
2023-08-04 09:15:06 +0900

    #S09P12D107-379 #in-progress #comment fix(UserSignInResDto) : profile img 응답 값 삭제

--- backend/src/main/java/com/weffy/user/dto/Response/UserSignInResDto.java
+++ backend/src/main/java/com/weffy/user/dto/Response/UserSignInResDto.java
@@ -7,14 +7,12 @@ import lombok.Setter;
 @Setter
 public class UserSignInResDto {
     String userId;
-    String profile_img;
     String accessToken;
     String refreshToken;
 
-    public UserSignInResDto of(String userId, String profile_img, String accessToken, String refreshToken) {
+    public UserSignInResDto of(String userId, String accessToken, String refreshToken) {
         UserSignInResDto res = new UserSignInResDto();
         res.userId = userId;
-        res.profile_img = profile_img;
         res.accessToken = accessToken;
         res.refreshToken = refreshToken;
         return res;
