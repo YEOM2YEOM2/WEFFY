1ef7b47b9ddd2d45e90b611875416fe26811f9a3
이서현 <kathyleesh7@gmail.com>
2023-08-05 04:33:14 +0900

    #S09P12D107-379 #in-progress #comment refactor(Token) : 회원가입, 로그인 속 토큰 저장 분리
    회원가입, 로그인에서 똑같이 생성 저장 되는 토큰 코드를 분리해서 중복 제거 및 IoC

--- backend/src/main/java/com/weffy/token/dto/response/CreateTokenResDto.java
+++ backend/src/main/java/com/weffy/token/dto/response/CreateTokenResDto.java
@@ -1,2 +1,19 @@
-package com.weffy.token.dto.response;public class CreateTokenResDto {
+package com.weffy.token.dto.response;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateTokenResDto {
+    String accessToken;
+    String refreshToken;
+
+    public CreateTokenResDto of(String accessToken, String refreshToken) {
+        CreateTokenResDto res = new CreateTokenResDto();
+        res.accessToken = accessToken;
+        res.refreshToken = refreshToken;
+        return res;
+    }
 }
--- backend/src/main/java/com/weffy/token/service/RefreshTokenService.java
+++ backend/src/main/java/com/weffy/token/service/RefreshTokenService.java
@@ -1,2 +1,12 @@
-package com.weffy.token.service;public interface RefreshTokenService {
+package com.weffy.token.service;
+
+import com.weffy.token.dto.response.CreateTokenResDto;
+import com.weffy.user.entity.WeffyUser;
+import net.bis5.mattermost.client4.ApiResponse;
+import net.bis5.mattermost.model.User;
+
+public interface RefreshTokenService {
+
+    void saveToken(WeffyUser weffyUser, String refreshToken);
+    CreateTokenResDto createUserToken(ApiResponse<User> userInfo, WeffyUser weffyUser);
 }
