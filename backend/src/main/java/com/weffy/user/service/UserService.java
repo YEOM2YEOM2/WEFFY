851784b4962bd2e56b1f46ad9208687efa9f9966
이서현 <kathyleesh7@gmail.com>
2023-08-04 13:18:15 +0900

    #S09P12D107-379 #in-progress #comment feat(User) : 메인화면에서 필요한 개인 정보 조회

--- backend/src/main/java/com/weffy/user/service/UserService.java
+++ backend/src/main/java/com/weffy/user/service/UserService.java
@@ -1,6 +1,8 @@
 package com.weffy.user.service;
 
 import com.weffy.user.dto.Request.UserSignInReqDto;
+import com.weffy.user.dto.Response.UserInfoResDto;
+import com.weffy.user.dto.Response.UserMainResDto;
 import com.weffy.user.dto.Response.UserSignInResDto;
 import com.weffy.user.entity.WeffyUser;
 
@@ -8,4 +10,7 @@ public interface UserService {
 
     UserSignInResDto signIn(UserSignInReqDto signinInfo, String role);
     WeffyUser findById(Long userId);
+
+    UserMainResDto mainUser(String userId);
+
 }
--- backend/src/main/java/com/weffy/user/service/UserServiceImpl.java
+++ backend/src/main/java/com/weffy/user/service/UserServiceImpl.java
@@ -8,6 +8,8 @@ import com.weffy.token.entity.RefreshToken;
 import com.weffy.token.repository.RefreshTokenRepository;
 import com.weffy.token.service.TokenService;
 import com.weffy.user.dto.Request.UserSignInReqDto;
+import com.weffy.user.dto.Response.UserInfoResDto;
+import com.weffy.user.dto.Response.UserMainResDto;
 import com.weffy.user.dto.Response.UserSignInResDto;
 import com.weffy.user.entity.Role;
 import com.weffy.user.entity.WeffyUser;
@@ -21,6 +23,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
+import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -48,7 +51,7 @@ public class UserServiceImpl implements UserService {
 
         ApiResponse<User> userInfo = mattermostHandler.login(userId, password);
         User mmClient = userInfo.readEntity();
-        String profile_img = mattermostHandler.image(mmClient.getId());
+        String profileImg = mattermostHandler.image(mmClient.getId());
 
         WeffyUser weffyUser;
         Optional<WeffyUser> existingUser = userRepository.findByIdentification(mmClient.getId());
@@ -62,7 +65,7 @@ public class UserServiceImpl implements UserService {
                         .nickname(mmClient.getNickname())
                         .role((role != null && role.equals("ADMIN"))? Role.ADMIN: Role.USER)
                         .active(true)
-                        .profile_img(profile_img)
+                        .profileImg(profileImg)
                         .build()
             );
         } else {
@@ -93,9 +96,16 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    public UserMainResDto mainUser(String identification) {
+        Optional<WeffyUser> user = Optional.ofNullable(userRepository.findByIdentification(identification).orElseThrow(() -> new NoSuchElementException("No user found with id " + identification)));
+        UserMainResDto userMainResDto = new UserMainResDto().of(user.get());
+        return userMainResDto;
+    }
+    @Override
     public WeffyUser findById(Long userId) {
         return userRepository.findById(userId)
                 .orElseThrow(() -> new IllegalArgumentException("User with id " + userId + " not found"));
     }
+
 
 }
