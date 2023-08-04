f9766bb1fb7a8864645f767d73b58cf66b0597c4
이서현 <kathyleesh7@gmail.com>
2023-08-05 04:33:14 +0900

    #S09P12D107-379 #in-progress #comment refactor(Token) : 회원가입, 로그인 속 토큰 저장 분리
    회원가입, 로그인에서 똑같이 생성 저장 되는 토큰 코드를 분리해서 중복 제거 및 IoC

--- backend/src/main/java/com/weffy/token/service/RefreshTokenServiceImpl.java
+++ backend/src/main/java/com/weffy/token/service/RefreshTokenServiceImpl.java
@@ -1,14 +1,58 @@
 package com.weffy.token.service;
 
+import com.weffy.mattermost.service.MattermostService;
+import com.weffy.token.config.TokenProvider;
+import com.weffy.token.dto.response.CreateTokenResDto;
 import com.weffy.token.entity.RefreshToken;
 import com.weffy.token.repository.RefreshTokenRepository;
+import com.weffy.user.entity.WeffyUser;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import net.bis5.mattermost.client4.ApiResponse;
+import net.bis5.mattermost.model.User;
 import org.springframework.stereotype.Service;
 
-@Service
+import java.time.Duration;
+import java.util.Objects;
+import java.util.Optional;
+
+@Service("RefreshTokenService")
 @RequiredArgsConstructor
-public class RefreshTokenService {
+public class RefreshTokenServiceImpl implements RefreshTokenService{
     private final RefreshTokenRepository refreshTokenRepository;
+    private final TokenProvider tokenProvider;
+    private final MattermostService mattermostService;
+
+    @Override
+    @Transactional
+    public void saveToken(WeffyUser weffyUser, String refreshToken) {
+        RefreshToken token = RefreshToken
+                .builder()
+                .weffyUser(weffyUser)
+                .refreshToken(refreshToken)
+                .build();
+
+        refreshTokenRepository.save(token);
+    }
+
+
+    @Override
+    public CreateTokenResDto createUserToken(ApiResponse<User> userInfo, WeffyUser weffyUser) {
+        // Mattermost 세션 토큰
+        String token = Objects.requireNonNull(userInfo.getRawResponse().getHeaders().get("Token").get(0).toString());
+        mattermostService.saveSession(weffyUser, token);
+        // accessToken
+        String accessToken = tokenProvider.generateToken(weffyUser,  Duration.ofHours(1));
+        //  refreshToken
+        String refreshToken = tokenProvider.generateToken(weffyUser,  Duration.ofDays(14));
+        Optional<RefreshToken> beforeToken = refreshTokenRepository.findByWeffyUser(weffyUser);
+        if ((beforeToken.isPresent())) {
+            beforeToken.get().updateToken(refreshToken);
+        } else {
+            saveToken(weffyUser, refreshToken);
+        }
+        return new CreateTokenResDto().of(accessToken, refreshToken);
+    }
 
     public RefreshToken findByRefreshToken(String refreshToken) {
         return refreshTokenRepository.findByRefreshToken(refreshToken)
