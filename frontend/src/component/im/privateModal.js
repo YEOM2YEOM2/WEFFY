1404e2f62c17c85df0603dcbc3bd32673272026b
thisiseunjin <hara0822@naver.com>
2023-08-06 23:42:56 +0900

    #S09P12D107-316 #done #comment feat(privateModule, redux) : 카메라, 닉네임 저장 리덕스추가 및 마이크, 카메라 설정 불러오기

--- frontend/src/component/im/privateModal.js
+++ frontend/src/component/im/privateModal.js
@@ -1,39 +1,96 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import styles from "./privateModal.module.css";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  setSession,
+  setNickname,
+  setMicrophones,
+  setCameras,
+  selectMicrophone,
+  selectCamera,
+  toggleMicStatus,
+  toggleCameraStatus,
+} from "../../store/setting.js";
 
-//mui 외부 라이브러리
+import { OpenVidu } from "openvidu-browser";
+
 import Avatar from "@mui/material/Avatar";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import Paper from "@mui/material/Paper";
-import { styled } from "@mui/material/styles";
-
-//기본 프로필 이미지
-import defaultImg from "../../assets/images/defualt_image.png";
-
-//icon
+import Stack from "@mui/material/Stack";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
 import CloseIcon from "@mui/icons-material/Close";
 import MicOffIcon from "@mui/icons-material/MicOff";
 import MicIcon from "@mui/icons-material/Mic";
 import VideocamIcon from "@mui/icons-material/Videocam";
 import VideocamOffIcon from "@mui/icons-material/VideocamOff";
-import { IconButton, Typography } from "@mui/material";
+import { IconButton } from "@mui/material";
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-}));
+import defaultImg from "../../assets/images/defualt_image.png";
 
 const PrivateModal = ({ handleClose }) => {
-  const [micStatus, setMicStatus] = useState(false);
-  const [cameraStatus, setCameraStatus] = useState(false);
-  const [nickname, setNickname] = useState("default nickname");
+  const dispatch = useDispatch();
+  const [nickname, setUserNickname] = useState("default nickname");
+  const [mics, setMics] = useState([]);
+  const [cams, setCams] = useState([]);
+  const OV = useRef(new OpenVidu()).current;
+  const micStatus = useSelector((state) => state.micStatus);
+  const cameraStatus = useSelector((state) => state.cameraStatus);
 
-  //openVidu
+  const handleMicStatusToggle = () => {
+    dispatch(toggleMicStatus());
+  };
+
+  const handleCameraStatusToggle = () => {
+    dispatch(toggleCameraStatus());
+  };
+
+  useEffect(() => {
+    dispatch(setSession("SessionA"));
+
+    OV.getDevices()
+      .then((devices) => {
+        console.log(devices);
+        const mics = devices.filter((device) => device.kind === "audioinput");
+        const cams = devices.filter((device) => device.kind === "videoinput");
+        dispatch(setMicrophones(mics));
+        dispatch(setCameras(cams));
+        if (mics.length > 0) {
+          setMics(mics);
+          dispatch(selectMicrophone(mics[0]));
+        }
+        if (cams.length > 0) {
+          setCameras(cams);
+          dispatch(selectCamera(cams[0]));
+        }
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  const handleNicknameChange = (e) => {
+    const newNickname = e.target.value;
+    if (!newNickname) {
+      console.error("닉네임을 비워둘 수는 없습니다.");
+      return;
+    }
+    setUserNickname(newNickname);
+    dispatch(setNickname(newNickname));
+  };
+
+  const handleSelectMicrophone = (event) => {
+    const selectedMic = mics.find((mic) => mic.deviceId === event.target.value);
+    dispatch(selectMicrophone(selectedMic));
+  };
+
+  const handleSelectCamera = (event) => {
+    const selectedCamera = cams.find(
+      (cam) => cam.deviceId === event.target.value
+    );
+    dispatch(selectCamera(selectedCamera));
+  };
 
   return (
     <div className={styles["modal"]} onClick={handleClose}>
@@ -47,37 +104,6 @@ const PrivateModal = ({ handleClose }) => {
           </IconButton>
         </div>
 
-        {/* <div style={{ display: "flex", justifyContent: "center" }}>
-          <Avatar
-            alt="profileImg"
-            src={defaultImg}
-            sx={{ width: 200, height: 200 }}
-          />
-        </div>
-        <div className={styles["settingContainer"]}>
-          <div style={{ display: "flex", justifyContent: "flex-start" }}>
-            <IconButton onClick={() => setMicStatus(!micStatus)}>
-              <Box borderRadius={3} className={styles["btnBox"]}>
-                {micStatus === true ? (
-                  <MicIcon style={{ color: "white", fontSize: 40 }} />
-                ) : (
-                  <MicOffIcon style={{ color: "#FF0E2B", fontSize: 40 }} />
-                )}
-              </Box>
-            </IconButton>
-
-            <IconButton onClick={() => setCameraStatus(!cameraStatus)}>
-              <Box borderRadius={3} className={styles["btnBox"]}>
-                {cameraStatus === true ? (
-                  <VideocamIcon style={{ color: "white", fontSize: 40 }} />
-                ) : (
-                  <VideocamOffIcon style={{ color: "#FF0E2B", fontSize: 40 }} />
-                )}
-              </Box>
-            </IconButton>
-          </div>
-          <Typography className={styles["nickname"]}>{nickname}</Typography>
-        </div> */}
         <Box sx={{ flexGrow: 1 }}>
           <Grid container spacing={2} columns={14}>
             <Grid item xs={6} className={styles["leftBox"]}>
@@ -92,14 +118,61 @@ const PrivateModal = ({ handleClose }) => {
                 <input
                   type="text"
                   value={nickname}
-                  onChange={(e) => setNickname(e.target.value)}
+                  onChange={handleNicknameChange}
                   className={styles["nickname"]}
                 />
               </Box>
             </Grid>
-            <Grid item xs={8}>
-              <Item>xs=8</Item>
-              <Item>xs=8</Item>
+
+            <Grid item xs={8} className={styles["selectContainer"]}>
+              <Stack spacing={10}>
+                <Grid>
+                  <FormControl className={styles["selectBox"]}>
+                    <Select
+                      value={mics.length > 0 ? mics[0].deviceId : ""}
+                      onChange={handleSelectMicrophone}
+                      displayEmpty
+                      inputProps={{ "aria-label": "Without label" }}
+                    >
+                      {mics.map((mic, index) => (
+                        <MenuItem key={index} value={mic.deviceId}>
+                          {mic.label || `Microphone ${index + 1}`}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+                  <IconButton onClick={handleMicStatusToggle}>
+                    {micStatus ? (
+                      <MicIcon />
+                    ) : (
+                      <MicOffIcon style={{ color: "red" }} />
+                    )}
+                  </IconButton>
+                </Grid>
+                <Grid>
+                  <FormControl className={styles["selectBox"]}>
+                    <Select
+                      value={cams.length > 0 ? cams[0].deviceId : ""}
+                      onChange={handleSelectCamera}
+                      displayEmpty
+                      inputProps={{ "aria-label": "Without label" }}
+                    >
+                      {cams.map((cam, index) => (
+                        <MenuItem key={index} value={cam.deviceId}>
+                          {cam.label}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+                  <IconButton onClick={handleCameraStatusToggle}>
+                    {cameraStatus ? (
+                      <VideocamIcon />
+                    ) : (
+                      <VideocamOffIcon style={{ color: "red" }} />
+                    )}
+                  </IconButton>
+                </Grid>
+              </Stack>
             </Grid>
           </Grid>
         </Box>
