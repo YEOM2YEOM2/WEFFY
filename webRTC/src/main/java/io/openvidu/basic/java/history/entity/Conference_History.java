328e2c74cc0074bc36fa550e4609ca1117af8587
Poposong <psi1908@naver.com>
2023-08-08 03:44:10 +0900

    #S09P12D107-400 #in-progress #comment type(Package) feat(conference) : DB 구현, 회의 상세 조회, 수정

---
+++ webRTC/src/main/java/io/openvidu/basic/java/history/entity/Conference_History.java
@@ -0,0 +1,37 @@
+package io.openvidu.basic.java.history.entity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+
+public class Conference_History {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String user_identification;
+
+    private Long conference_id;
+
+    @Enumerated(value = EnumType.STRING)
+    private Active active;
+
+    private Date insert_time;
+
+    @Builder
+    public Conference_History(Long id, String user_identification, Long conference_id, Active active, Date insert_time) {
+        this.id = id;
+        this.user_identification = user_identification;
+        this.conference_id = conference_id;
+        this.active = active;
+        this.insert_time = insert_time;
+    }
+}
