4bec8304dbb4fb412f9015fd970f0726f8d00c34
이서현 <kathyleesh7@gmail.com>
2023-08-07 17:09:33 +0900

    #S09P12D107-408 #in-progress #comment feat(Exception) : Mattermost 예외처리 구현
    Mattermost 로그인과 이미지 가져올 때 예외 처리 구현

--- backend/src/main/java/com/weffy/exception/ExceptionEnum.java
+++ backend/src/main/java/com/weffy/exception/ExceptionEnum.java
@@ -9,7 +9,9 @@ public enum ExceptionEnum {
     USEREXIST(HttpStatus.BAD_REQUEST,4000,"회원이 존재합니다."),
     USERNOTEXIST(HttpStatus.BAD_REQUEST,4001,"회원이 존재하지 않습니다."),
     USERWITHDRAW(HttpStatus.BAD_REQUEST,4002,"탈퇴한 회원입니다."),
-    INVALIDUSER(HttpStatus.BAD_REQUEST,4003,"올바르지 않은 사용자의 접근입니다."),;
+    INVALIDUSER(HttpStatus.BAD_REQUEST,4003,"올바르지 않은 사용자의 접근입니다."),
+    MATTERMOSTLOGINFAILED(HttpStatus.BAD_REQUEST,4004,"mattermost 로그인 실패"),
+    IMAGENOTFOUND(HttpStatus.BAD_REQUEST,4005,"mattermost 이미지가 존재하지 않습니다."),;
 
 
     private HttpStatus status;
