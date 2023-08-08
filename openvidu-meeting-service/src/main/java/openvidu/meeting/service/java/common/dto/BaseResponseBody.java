185990e058b4ac27c5f51301708ddd678c4356c5
Poposong <psi1908@naver.com>
2023-08-08 10:29:27 +0900

    #S09P12D107-400 #in-progress #comment type(Package) fix(conference) : Conference Entity 수정,Conference 비활성화 API

---
+++ openvidu-meeting-service/src/main/java/openvidu/meeting/service/java/common/dto/BaseResponseBody.java
@@ -0,0 +1,21 @@
+package openvidu.meeting.service.java.common.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BaseResponseBody<T> {
+
+    private int status;
+    private T data;
+
+    // getters and setters
+
+    public static <T> BaseResponseBody<T> of(int status, T data) {
+        BaseResponseBody<T> body = new BaseResponseBody<>();
+        body.setStatus(status);
+        body.setData(data);
+        return body;
+    }
+}
--- webRTC/src/main/java/io/openvidu/basic/java/history/repository/HistoryRepository.java
+++
@@ -1,8 +0,0 @@
-package io.openvidu.basic.java.history.repository;
-
-import io.openvidu.basic.java.history.entity.Conference_History;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface HistoryRepository extends JpaRepository<Conference_History, Long>{
-}
