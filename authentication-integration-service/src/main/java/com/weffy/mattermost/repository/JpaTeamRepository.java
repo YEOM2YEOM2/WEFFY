6306a2f96a40c713ad575f73222687eb6e67e962
이서현 <kathyleesh7@gmail.com>
2023-08-10 08:56:46 +0900

    #S09P12D107-541 #in-progress #comment fix(Team) : 해당 팀에 user가 존재하지 않을 경우만 저장
    로그인 시 정보 업데이트

--- authentication-integration-service/src/main/java/com/weffy/mattermost/repository/JpaTeamRepository.java
+++ authentication-integration-service/src/main/java/com/weffy/mattermost/repository/JpaTeamRepository.java
@@ -2,8 +2,11 @@ package com.weffy.mattermost.repository;
 
 import com.weffy.mattermost.entity.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+
+@Repository
 
 public interface JpaTeamRepository extends JpaRepository<Team, Long> {
     Optional<Team> findByIdentification(String identification);
--- authentication-integration-service/src/main/java/com/weffy/mattermost/repository/JpaUserTeamRepository.java
+++ authentication-integration-service/src/main/java/com/weffy/mattermost/repository/JpaUserTeamRepository.java
@@ -1,7 +1,14 @@
 package com.weffy.mattermost.repository;
 
+import com.weffy.mattermost.entity.Team;
 import com.weffy.mattermost.entity.WeffyUserTeam;
+import com.weffy.user.entity.WeffyUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface JpaUserTeamRepository extends JpaRepository<WeffyUserTeam, Long> {
+        Optional<WeffyUserTeam> findByTeamAndWeffyUser(Team team, WeffyUser weffyUser);
 }
