9430adccb9676f7adc061ef1c5be75d103939c36
이서현 <kathyleesh7@gmail.com>
2023-08-11 16:26:22 +0900

    #S09P12D107-547 #in-progress #comment fix(File) : file 목록 조회 요청사항에 맞게 변경
    해당 user의 session token과 conference id도 추가 반환

--- backend/authentication-integration-service/src/main/java/com/weffy/file/service/FileServiceImpl.java
+++ backend/authentication-integration-service/src/main/java/com/weffy/file/service/FileServiceImpl.java
@@ -4,19 +4,15 @@ import com.weffy.file.entity.Files;
 import com.weffy.file.dto.request.FileReqDto;
 import com.weffy.file.dto.response.FileResDto;
 import com.weffy.file.dto.response.GetFileDto;
-import org.springframework.core.io.InputStreamResource;
 import org.springframework.stereotype.Service;
 import com.weffy.file.repository.JpaFileRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
-import software.amazon.awssdk.core.ResponseInputStream;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.core.sync.ResponseTransformer;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
-import software.amazon.awssdk.services.s3.model.GetObjectResponse;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-import software.amazon.awssdk.services.s3.model.S3Object;
 
 
 import javax.imageio.ImageIO;
