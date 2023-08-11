a185df2bfd1fe53793053f794764623d7e798bc9
이서현 <kathyleesh7@gmail.com>
2023-08-12 08:31:59 +0900

    #S09P12D107-544 #in-progress #comment feat(Question) : question 조회기능 추가

--- backend/openvidu-content-service/src/main/java/com/weffy/question/Repository/JpaQuestionRepository.java
+++ backend/openvidu-content-service/src/main/java/com/weffy/question/Repository/JpaQuestionRepository.java
@@ -4,9 +4,9 @@ import com.weffy.question.entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface JpaQuestionRepository extends JpaRepository<Question, Long> {
-
+    List<Question> findByConferenceId(String conferenceId);
 }
