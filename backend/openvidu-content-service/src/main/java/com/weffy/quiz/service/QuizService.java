ea0a75783932c5c2b1ce1883255fd9ba1acc0e75
이서현 <kathyleesh7@gmail.com>
2023-08-12 15:30:18 +0900

    #S09P12D107-547 #in-progress #comment feat(Quiz) : conference 별 quiz 질문 조회 기능 추가

--- backend/openvidu-content-service/src/main/java/com/weffy/quiz/service/QuizService.java
+++ backend/openvidu-content-service/src/main/java/com/weffy/quiz/service/QuizService.java
@@ -3,6 +3,10 @@ package com.weffy.quiz.service;
 import com.weffy.quiz.dto.request.QuizReqDto;
 import com.weffy.quiz.dto.response.QuizResDto;
 
+import java.util.List;
+
 public interface QuizService {
     QuizResDto createQuiz(QuizReqDto quizReqDto);
+
+    List<QuizResDto> getQuiz(String conferenceId);
 }
