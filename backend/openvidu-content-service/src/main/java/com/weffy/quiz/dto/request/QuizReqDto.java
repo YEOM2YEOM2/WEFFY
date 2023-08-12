31f45d0e02130a816704beb169602c6e5ddbff5d
이서현 <kathyleesh7@gmail.com>
2023-08-12 10:15:44 +0900

    #S09P12D107-547 #in-progress #comment feat(Quiz) : quiz entity 및 dto 생성

--- backend/openvidu-content-service/src/main/java/com/weffy/quiz/dto/request/QuizReqDto.java
+++ backend/openvidu-content-service/src/main/java/com/weffy/quiz/dto/request/QuizReqDto.java
@@ -6,4 +6,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class QuizReqDto {
+    String senderId;
+    String conferenceId;
+    String content;
 }
--- backend/openvidu-content-service/src/main/java/com/weffy/quiz/entity/Quiz.java
+++ backend/openvidu-content-service/src/main/java/com/weffy/quiz/entity/Quiz.java
@@ -1,14 +1,38 @@
 package com.weffy.quiz.entity;
 
-import jakarta.persistence.Entity;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Quiz {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String senderId;
+    private String conferenceId;
+
+    @Column(columnDefinition = "LONGTEXT")
+    private String content;
+
+    private Long parent;
+
+    @CreatedDate
+    private LocalDateTime sendAt;
+
+    @Builder
+    public Quiz(String senderId, String conferenceId, String content, Long parent, LocalDateTime sendAt) {
+        this.senderId = senderId;
+        this.conferenceId = conferenceId;
+        this.content = content;
+        this.parent = parent;
+        this.sendAt = sendAt;
+    }
 }
