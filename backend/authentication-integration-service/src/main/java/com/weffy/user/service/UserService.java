c552285aedc25b61f022a7060a1829c12d53ae97
이서현 <kathyleesh7@gmail.com>
2023-08-13 05:08:42 +0900

    #S09P12D107-248 #in-progress #comment refactor(User) : 코드 개선 및 swagger 기능 강화

--- backend/authentication-integration-service/src/main/java/com/weffy/user/service/UserService.java
+++ backend/authentication-integration-service/src/main/java/com/weffy/user/service/UserService.java
@@ -12,18 +12,23 @@ import java.io.IOException;
 
 public interface UserService {
 
+    // 회원가입
     void signUp(UserSignInReqDto signinInfo, String role) throws IOException;
-
+    // 회원 가입 시 Mattermost nickname으로 부터 권한 부여
     Role getRole(String nickName);
+    // 로그인
     UserSignInResDto signIn(UserSignInReqDto signinInfo) throws IOException, InterruptedException;
+    // repository 예외 처리
     WeffyUser findById(Long userId);
     WeffyUser findByEmail(String email);
+    // 메인화면에서 필요한 user 정보 조회
     UserMainResDto mainUser(String userId);
+    // user 정보 조회
     UserInfoResDto getUser(WeffyUser weffyUser);
-
+    // 이미지와 nickname 수정
     void setUser(WeffyUser weffyUser, MultipartFile profileImg, String nickName);
-
+    // password 변경
     void setPassword(WeffyUser weffyUser, String password);
-
+    // 회원 탈퇴(비활성)
     void deleteUser(WeffyUser weffyUser);
 }
