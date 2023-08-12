d995554f99adeb89373de6909d36a88d0fb44626
이서현 <kathyleesh7@gmail.com>
2023-08-12 20:20:00 +0900

    #S09P12D107-548 #in-progress #comment feat(Role) : role 세분화
    회원가입시 role 세분화(관리자, 코치, 컨설턴트, 프로, 강사, 학생)

--- backend/authentication-integration-service/src/main/java/com/weffy/user/service/UserService.java
+++ backend/authentication-integration-service/src/main/java/com/weffy/user/service/UserService.java
@@ -4,16 +4,17 @@ import com.weffy.user.dto.Request.UserSignInReqDto;
 import com.weffy.user.dto.Response.UserInfoResDto;
 import com.weffy.user.dto.Response.UserMainResDto;
 import com.weffy.user.dto.Response.UserSignInResDto;
+import com.weffy.user.entity.Role;
 import com.weffy.user.entity.WeffyUser;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.Optional;
 
 public interface UserService {
 
     void signUp(UserSignInReqDto signinInfo, String role) throws IOException;
+
+    Role getRole(String nickName);
     UserSignInResDto signIn(UserSignInReqDto signinInfo) throws IOException, InterruptedException;
     WeffyUser findById(Long userId);
     WeffyUser findByEmail(String email);
