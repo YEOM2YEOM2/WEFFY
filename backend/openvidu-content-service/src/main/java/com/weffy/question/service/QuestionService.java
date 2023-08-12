5e129caba8cdae67575146952cb9d08a4e07e312
이서현 <kathyleesh7@gmail.com>
2023-08-12 09:31:20 +0900

    #S09P12D107-544 #in-progress #comment refactor(Question) : URL패턴 명확하게 변경

--- backend/openvidu-content-service/src/main/java/com/weffy/question/service/QuestionService.java
+++ backend/openvidu-content-service/src/main/java/com/weffy/question/service/QuestionService.java
@@ -3,7 +3,6 @@ package com.weffy.question.service;
 import com.weffy.question.dto.request.QuestionReqDto;
 import com.weffy.question.dto.response.QuestionResDto;
 import com.weffy.question.dto.response.QuestionStateResDto;
-import com.weffy.question.entity.Question;
 
 import java.util.List;
 
