9f5bc9a377cd381b86522d660670d98c47e12b6e
이서현 <kathyleesh7@gmail.com>
2023-08-14 10:32:35 +0900

    fix(anotation) : 어노테이션 추가

--- backend/authentication-integration-service/src/main/java/com/weffy/token/service/TokenServiceImpl.java
+++ backend/authentication-integration-service/src/main/java/com/weffy/token/service/TokenServiceImpl.java
@@ -5,11 +5,13 @@ import com.weffy.token.config.TokenProvider;
 import com.weffy.token.repository.RefreshTokenRepository;
 import com.weffy.user.entity.WeffyUser;
 import com.weffy.user.service.UserService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
 
+@Transactional
 @Service("TokenService")
 @RequiredArgsConstructor
 public class TokenServiceImpl implements TokenService{
