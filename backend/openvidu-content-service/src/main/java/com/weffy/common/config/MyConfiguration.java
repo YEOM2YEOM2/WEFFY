69f65cfd13101e0a958bdc67f6700fbf54285a93
이서현 <kathyleesh7@gmail.com>
2023-08-18 09:02:56 +0900

    fix(cors) : cors 추가

--- backend/openvidu-content-service/src/main/java/com/weffy/common/config/MyConfiguration.java
+++ backend/openvidu-content-service/src/main/java/com/weffy/common/config/MyConfiguration.java
@@ -11,7 +11,7 @@ public class MyConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 요청 경로에 대해
-                .allowedOrigins("http://localhost:3000", "http://i9d107.p.ssafy.io:3000",  "https://3.39.223.169", "https://i9d107.p.ssafy.io") // 허용할 오리진
+                .allowedOrigins("http://localhost:3000", "http://i9d107.p.ssafy.io:3000",  "https://3.39.223.169", "https://i9d107.p.ssafy.io", "http://ssafy-weffy.s3-website.ap-northeast-2.amazonaws.com/") // 허용할 오리진
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS"); // 허용할 HTTP 메서드
     }
 }
